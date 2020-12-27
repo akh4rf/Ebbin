4fc458eca8aa648b816f39fec1427473c883091f
akh4rf <akh4rf@virginia.edu>
2020-12-26 23:54:48 -0500

    End of day seven!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 function menuBtnFunc(x) {
   x.classList.toggle("change");
+
+  var y = document.getElementsByClassName("navbar-buttons");
+  for (let item of y) {
+    item.classList.toggle("show-nav");
+  }
 }
